6ae929a3a3fad5b8f17ebad101ecc06d5f7327ae
Dongsik Ga <ret0422@gmail.com>
2021-01-21 18:27:42 +0900

    Remove PT & AI Button

--- src/components/profile/FitclassContentsMenu.tsx
+++ src/components/profile/FitclassContentsMenu.tsx
@@ -10,59 +10,19 @@ import {WIDTH} from '@constants/dimensions';
 
 function FitclassContentsMenu() {
   return (
-    <View>
-      <View style={{flexDirection: 'row'}}>
-        <TouchableOpacity onPress={null}>
-          <ImageBackground
-            style={styles.backgroundimage1}
-            source={require('@assets/image/profile_thumbnail_1.jpg')}
-            resizeMode="cover">
-            <View style={styles.backgroundcover}>
-              <Text
-                style={[
-                  styles.maintitle,
-                  {textAlign: 'right', marginRight: 5},
-                ]}>
-                FITCLASS 캘린더
-              </Text>
-            </View>
-          </ImageBackground>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={null}>
-          <ImageBackground
-            style={styles.backgroundimage1}
-            source={require('@assets/image/posture_service_thumbnail.png')}
-            resizeMode="cover">
-            <View style={styles.backgroundcover}>
-              <Text
-                style={[styles.subtitle, {textAlign: 'right', marginRight: 5}]}>
-                FITCLASS AI
-              </Text>
-              <Text
-                style={[
-                  styles.maintitle,
-                  {textAlign: 'right', marginRight: 5},
-                ]}>
-                FITCLASS 자세교정
-              </Text>
-            </View>
-          </ImageBackground>
-        </TouchableOpacity>
-      </View>
-      <TouchableOpacity onPress={null}>
-        <ImageBackground
-          style={styles.backgroundimage2}
-          source={require('@assets/image/pt_service_thumbnail.jpg')}
-          resizeMode="cover">
-          <View style={styles.backgroundcover}>
-            <Text
-              style={[styles.maintitle, {textAlign: 'right', marginRight: 5}]}>
-              FITCLASS PT 지도
-            </Text>
-          </View>
-        </ImageBackground>
-      </TouchableOpacity>
-    </View>
+    <TouchableOpacity onPress={null}>
+      <ImageBackground
+        style={styles.backgroundimage1}
+        source={require('@assets/image/profile_thumbnail_1.jpg')}
+        resizeMode="cover">
+        <View style={styles.backgroundcover}>
+          <Text
+            style={[styles.maintitle, {textAlign: 'right', marginRight: 5}]}>
+            FITCLASS 캘린더
+          </Text>
+        </View>
+      </ImageBackground>
+    </TouchableOpacity>
   );
 }
 
@@ -74,11 +34,6 @@ const styles = StyleSheet.create({
     marginBottom: 30,
   },
   backgroundimage1: {
-    width: WIDTH / 2,
-    height: 100,
-    marginTop: 10,
-  },
-  backgroundimage2: {
     height: 100,
   },
   backgroundcover: {
@@ -89,13 +44,7 @@ const styles = StyleSheet.create({
   },
   maintitle: {
     color: '#ffffff',
-    fontSize: 22,
+    fontSize: 25,
     fontFamily: 'OpenSauceSans-Bold',
-  },
-  subtitle: {
-    color: '#ffffff',
-    fontFamily: 'OpenSauceSans-Bold',
-    fontSize: 12,
-    marginTop: 2,
   },
 });
